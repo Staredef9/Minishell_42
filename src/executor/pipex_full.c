1040f1631872f03d6027b95e84933ae2f333faeb
Federico Salvetti <salvettifederico68@gmail.com>
2023-06-16 22:09:19 +0200

    aggiunta libft

--- src/executor/pipex_full.c
+++ src/executor/pipex_full.c
@@ -1,6 +1,14 @@
 #include "minishell.h"
 
 
+//mi aspetto che la parte del retrieving degli argomenti venga gestita dal parser e lexer, 
+//per cui imposto le funzioni di modo che ricevano gli argomenti gia' sistemati dalle precedenti.
+
+
+
+
+
+
 // qua va implementata PIPEX con i bonus 
 //
 // come va gestita pipex qui?
@@ -13,9 +21,6 @@
 // Se l'executor vede che e'necessario attivare una pipe, chiama le funzioni di pipex completo, 
 // executor gestisce la chiamata a pipe appena ne vede una sola, e poi pipex gestisce anche se ci sono 
 // pipe multiple, oppure la logica delle pipe multiple va gestita da executor??
-//
-//
-//
 //
 // Le variabili ambientali, invece che farne il parsing dentro pipex, vediamo di averle
 // in una struttura fin da quando viene attivato il programma, di modo da averle comode sempre come riferimento 
