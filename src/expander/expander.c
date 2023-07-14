75d6413dfa4770d30e500b6c838986ce0df98fb6
Neri Cortigiani <newi.corty@gmail.com>
2023-07-14 17:28:30 +0200

    exp

--- src/expander/expander.c
+++ src/expander/expander.c
@@ -1,1 +1,7 @@
 #include "./executor/minishell.h"
+//Deve sostituire tutte le variabili espandibili 
+//con il loro valore effettivo
+//ES: echo "ciao $HOME" -> echo "ciao /nfs/homes/ncortigi"
+
+void	expander(t_data *data, char *input)
+{}
